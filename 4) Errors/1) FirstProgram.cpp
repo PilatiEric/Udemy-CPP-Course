d5ae31173669934f4e201a8ff9774a155c0ceebc
Eric <pilatieric@gmail.com>
2023-10-28 00:19:30 -0400

    Minor change

---
+++ 4) Errors/1) FirstProgram.cpp
@@ -0,0 +1,31 @@
+#pragma region Inlude & Using
+//Include
+#include <iostream>
+
+//Using
+using namespace std;
+
+#pragma endregion
+
+#pragma region Function Prototypes
+//Function prototypes
+int FaveNumber();
+
+#pragma endregion
+
+//All C++ programs must have a main()
+//All C++ programs may only have one main()
+int main()
+{
+    FaveNumber();
+    return 0;
+}
+
+int FaveNumber()
+{
+    int Number;
+    cout << "Enter your favorite number between 1 and 100" << endl;
+    cin >> Number;
+    cout << "Your favorite number is " << Number << "." << endl << endl;
+    return 0;
+}
