f18abc0aba444577bdd9e2c90df1ecae9e470dd3
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-11-04 18:28:49 -0400

    New Notes

---
+++ 6) Variables/1)Global Variables.cpp
@@ -0,0 +1,5 @@
+/************************************
+ * Global Variables Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535362#questions
+*************************************/
