61618ce6cb7667ab9038d9a4f61a3078f1a4bc59
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-11-25 23:49:00 -0500

    Update 1) Expressions and Statements.cpp

--- 8) Statements and Operators/1) Expressions and Statements.cpp
+++ 8) Statements and Operators/1) Expressions and Statements.cpp
@@ -1,8 +1,8 @@
-/****************************************
+/*****************************************
  * Expressions and Statements Notes
  * 
  * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535410#notes
-*****************************************/
+******************************************/
 
 #include <iostream>
 #include <vector>
