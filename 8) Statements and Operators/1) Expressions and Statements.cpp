6497fd4feb0564e285d804b5ee146b4f75334875
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-11-29 21:18:17 -0500

    New lesson notes

--- 8) Statements and Operators/1) Expressions and Statements.cpp
+++ 8) Statements and Operators/1) Expressions and Statements.cpp
@@ -4,18 +4,28 @@
  * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535410#notes
 *****************************************/
 
-#include <iostream>
-#include <vector>
-using namespace std;
 
-#pragma region Function Prototypes
+/*
+What is an Expression?
+------------------------------------
+-Most basic building block of a program
+-Computes a value from a number of operands
+-Examples
+    - 34 value is equal to 34    //literal
+    - favorite_number            //variable
+    - 1.5 + 2.8                  //addition
+    - 2 * 5                      //multiplication
+    - a > b                      //relational
+    - a = b                      //assignment
 
-
-
-#pragma endregion
-
-
-int main()
-{
-    return 0;
-}
+What is a Statement?
+------------------------------------
+-A complete line of code that performs some action
+-Usually terminated with a semicolon
+-Examples
+    - int x;                                  //declaration
+    - favorite_number = 12                    //assignment
+    - 1.5 * 2.8;                              //expression
+    - x = 2 * 5                               //assignment
+    -if (a > b) cout << "a is great than b"   //if
+*/
