11cacbe7001162b635cbfeaa32367109f3dcde88
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-11-29 21:58:43 -0500

    Edited notes

--- 8) Statements and Operators/2) Using Operators.cpp
+++ 8) Statements and Operators/2) Using Operators.cpp
@@ -4,20 +4,6 @@
  * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535412#notes
 ***********************************************/
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-#pragma region Function Prototypes
-
-
-
-#pragma endregion
-
-int main()
-{
-    return 0;
-}
 
 /*
 Using Operators
