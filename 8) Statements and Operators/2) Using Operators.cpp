cdc17a6a9d549a2af37d15ca197841d95f7c35b0
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-11-29 21:53:45 -0500

    New lesson notes

---
+++ 8) Statements and Operators/2) Using Operators.cpp
@@ -0,0 +1,37 @@
+/**********************************************
+ * Using Operators Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535412#notes
+***********************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+int main()
+{
+    return 0;
+}
+
+/*
+Using Operators
+-----------------------------------
+-Sets of operators
+    -unary
+    -binary
+    -ternary
+-Common operators that can be grouped
+    -assignment
+    -arithmetic
+    -increment/decrement
+    -relational
+    -logical
+    -member access
+    -other
+*/
