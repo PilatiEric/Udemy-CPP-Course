9d383e3b4ee084552e72ddbcfbd414f4d7b4ff79
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-12-01 00:18:26 -0500

    New lesson notes

---
+++ 8) Statements and Operators/4) Arithmetic Operators.cpp
@@ -0,0 +1,21 @@
+/***************************************
+ * Arithmetic Operators Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535416#notes
+****************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
