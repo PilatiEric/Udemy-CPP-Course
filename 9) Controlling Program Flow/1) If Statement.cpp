ed3770a8e4c379c4bb2e399128bba5e1655cf3d0
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-12-05 00:03:08 -0500

    New lesson notes

---
+++ 9) Controlling Program Flow/1) If Statement.cpp
@@ -0,0 +1,20 @@
+/******************************************
+ * If Statement Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535442#notes
+*******************************************/
+
+#include <iostream>
+using namespace std;
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
