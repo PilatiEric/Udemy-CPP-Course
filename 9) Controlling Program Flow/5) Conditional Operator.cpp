6fe4f929016efb3b72edfbe8045a242c7a5894bf
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-12-05 20:11:58 -0500

    New lesson notes

---
+++ 9) Controlling Program Flow/5) Conditional Operator.cpp
@@ -0,0 +1,18 @@
+/******************************************
+ * Conditional Operator Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535450#notes
+*******************************************/
+
+/*
+Conditional Operator
+------------------------------
+(conditional_operator) ? expression1 : expression2
+-Evaluates to a boolean
+    -If the conditional_operator is true, then the value of expression1 is returned
+    -If the conditional_operator is false, then the values of expression2 is returned
+-Similar to If-Else construct
+-Ternary Operator
+-Very useful when use inline
+-Very easy to abuse
+*/
