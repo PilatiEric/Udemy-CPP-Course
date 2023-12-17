fa311664e09d7a64e2ac991c81cb84c5fdd466b1
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-12-16 22:19:07 -0500

    Update 5) Conditional Operator.cpp

--- 9) Controlling Program Flow/5) Conditional Operator.cpp
+++ 9) Controlling Program Flow/5) Conditional Operator.cpp
@@ -10,9 +10,9 @@ Conditional Operator
 (conditional_operator) ? expression1 : expression2
 -Evaluates to a boolean
     -If the conditional_operator is true, then the value of expression1 is returned
-    -If the conditional_operator is false, then the values of expression2 is returned
--Similar to If-Else construct
+    -If the conditional_operator is false, then the value of expression2 is returned
+-Similar to the If-Else construct
 -Ternary Operator
--Very useful when use inline
+-Very useful when using inline
 -Very easy to abuse
 */
