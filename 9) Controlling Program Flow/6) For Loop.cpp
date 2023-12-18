f7cf5f62e7b62a5281a67f5ed490883636ee2f10
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-12-17 23:46:47 -0500

    New lesson notes

---
+++ 9) Controlling Program Flow/6) For Loop.cpp
@@ -0,0 +1,22 @@
+/******************************************
+ * For Loops Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535454#notes
+*******************************************/
+
+#include <iostream>
+using namespace std;
+
+#pragma region Function Prototypes
+
+int for_loop();
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
+
+
---
+++ 9) Controlling Program Flow/7) Ranged-based For Loop.cpp
@@ -0,0 +1,71 @@
+/******************************************
+ * Range-based For Loops Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535456#notes
+*******************************************/
+
+#include <iostream>
+#include <vector>
+#include <format>
+using namespace std;
+
+#pragma region Function Prototypes
+
+int range_loop();
+
+#pragma endregion
+
+
+int main()
+{
+    range_loop();
+    return 0;
+}
+
+
+/*
+Range-based For Loops
+--------------------------
+- for (var_type var_name: sequence)
+  {
+    statement;
+  }    
+
+    -var_name hold the current value in the sequence
+        -If it is a vector, it will be whichever value is currently indexed at
+        -If it is a string, it will loop through the individual chars
+            -var_name will be the char indexed
+        
+*/
+
+
+int range_loop()
+{
+    //Scores
+    int scores[] {10, 20, 30};
+
+    for (auto score: scores)
+        cout << score << endl << endl;
+        
+    
+
+    //Average Temperature
+    vector<double> temperatures {87.9, 77.9, 80.0, 72.5};
+    double average_temperature {};
+    double total {};
+
+    for (auto temp: temperatures)
+        total += temp;
+    
+    if (temperatures.size() != 0)
+        average_temperature = total / temperatures.size();
+    
+
+    cout << "Average Temperature: " << average_temperature << endl << endl;
+
+    //
+    for (auto val: {1, 2, 3, 4, 5})
+        cout << val << endl;
+    
+    return 0;
+}
