59433761b6bacf6c4a0c4eaddb6f19efb29f751f
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-01-06 18:49:19 -0500

    New Lesson Notes

---
+++ 10) Characters and Strings/1) Character Function.cpp
@@ -0,0 +1,63 @@
+/*********************************************
+ * Character Functions Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535476#overview
+**********************************************/
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
+
+
+/*
+Character Functions
+--------------------------------
+- #include <cctype>
+    -Functions for testing characters
+    -Functions for converting character case
+    - function_name(char)
+    - Functions
+        -Evaluation Functions
+            -isalpha(c)
+                -True if c is a letter
+            -isalnum(c)
+                -True if c is a leter or digit
+            -isdigit(c)
+                -True if c is a digit
+            -islower(c)
+                -True if c is a lowercase letter
+            -isupper(c)
+                -True if c is an uppercase letter
+            -isprint(c)
+                -True if c is a printable character
+            -ispunct(c)
+                -True if c is a punctuation character
+            -isspace(c)
+                -True if c is whitespace 
+        -Conversion Functions
+            -tolower(c)
+                -Returns lowercase of c
+            -toupper(c)
+                -Returns uppercase of c
+        
+
+
+
+
+
+*/
---
+++ 10) Characters and Strings/2) C-style Strings.cpp
@@ -0,0 +1,33 @@
+/*********************************************
+ * C-Style Strings Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535478#overview
+**********************************************/
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
+
+
+/*
+C-Style Strings
+-----------------------------------
+-
+
+
+*/
