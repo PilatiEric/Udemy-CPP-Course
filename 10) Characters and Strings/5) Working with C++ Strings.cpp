0aebaa617bbc626bfc5bce575c64c99d300bc3d2
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-01-12 05:29:24 -0500

    New Lesson Notes

---
+++ 10) Characters and Strings/5) Working with C++ Strings.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************************************
+ * Working with C++ Strings Notes
+ * 
+ * From: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535484#questions
+*******************************************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
+
+
+
+/*
+Working with C++ Strings
+---------------------------------------------
+*/
