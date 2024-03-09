b4415074f209bbd235f65222b9e5d73fbd33fa0b
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-03-08 23:26:31 -0500

    New Lesson Notes

--- 12) Pointers and References/7) Pointer Arithmetic.cpp
+++ 12) Pointers and References/7) Pointer Arithmetic.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 void compare_pointers();
 void comparing_values_at_pointer();
+void pointer_arithmetic();
 
 #pragma endregion
 
@@ -26,6 +27,7 @@ int main()
     cout << boolalpha;
     compare_pointers();
     comparing_values_at_pointer();
+    pointer_arithmetic();
     return 0;
 }
 
@@ -116,6 +118,31 @@ void pointer_arithmetic()
 
     //EXAMPLE 3
     cout << "EXAMPLE 3\n-----------------------" << endl;
-    
+    string s1 {"Frank"};
+    string s2 {"Frank"};
+    string s3 {"James"};
+    string *p1 {&s1};
+    string *p2 {&s2};
+    string *p3 {&s1};
 
+    cout << p1 << " == " << p2 << ": " << (p1 == p2) << endl;
+    cout << p1 << " == " << p3 << ": " << (p1 == p3) << endl;
+
+    cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl;
+    cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl;
+
+    p3 = &s3; //James
+    cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl << endl;
+
+    //EXAMPLE 4
+    cout << "EXAMPLE 4\n-----------------------" << endl;
+    char name[] {"Frank"};
+
+    char *char_ptr1 {nullptr};
+    char *char_ptr2 {nullptr};
+
+    char_ptr1 = &name[0];    //F
+    char_ptr2 = &name[3];   //n
+
+    cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2 - char_ptr1) << " characters away from " << *char_ptr1 << "." << endl << endl;
 }
