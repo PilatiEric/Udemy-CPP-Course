cc0b8a9dd7df24026afe5d22ead7e05c9459460d
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-03-09 23:57:54 -0500

    New Lesson Notes

---
+++ 12) Pointers and References/8) Const and Pointers.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************
+ * Const and Pointers Notes
+ * 
+ * FROM: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535540#questions/7245890
+ * 
+ * SUMMARY:
+******************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+#pragma region Function Prototypes
+
+
+
+#pragma endregion
+
+
+int main()
+{
+    return 0;
+}
