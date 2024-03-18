f0dd5b62c5f53a5ff5689817e4c5b79235f5dd89
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-03-17 23:23:27 -0400

    New Lesson Notes

--- 12) Pointers and References/9) Passing Pointers to Functions.cpp
+++ 12) Pointers and References/9) Passing Pointers to Functions.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 #pragma region Function Prototypes
 
-
+void double_data(int *int_ptr);
 
 #pragma endregion
 
@@ -21,5 +21,29 @@ using namespace std;
 
 int main()
 {
+    int value {10};
+
+    cout << value << endl << endl;
+
+    double_data(&value);
+
+    cout << value << endl << endl;
     return 0;
+}
+
+
+/*
+Passing pointers to a function
+-------------------------------------------------
+-Pass-by-reference with pointer parameters
+    -We can use pointers and the dereference operator to achieve pass-by-reference
+    -The function parameter is a pointer
+    -The actual parameter can be a pointer or address of a variable
+
+*/
+
+void double_data(int *int_ptr)
+{
+    cout << "double_data() was used\n----------------------------------------" << endl;
+    *int_ptr *= 2;
 }
