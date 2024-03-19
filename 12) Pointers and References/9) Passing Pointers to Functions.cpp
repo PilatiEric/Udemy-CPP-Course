b3512465d8b4b7cd0dc8b066c689173c30598875
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2024-03-18 21:22:44 -0400

    New Lesson Notes

--- 12) Pointers and References/9) Passing Pointers to Functions.cpp
+++ 12) Pointers and References/9) Passing Pointers to Functions.cpp
@@ -7,6 +7,8 @@
 ******************************************************************************************/
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,6 +16,8 @@ using namespace std;
 #pragma region Function Prototypes
 
 void double_data(int *int_ptr);
+void pass_ptr_in(int *int_ptr, int &value);
+void swap(int *a, int *b);
 
 #pragma endregion
 
@@ -22,18 +26,29 @@ void double_data(int *int_ptr);
 int main()
 {
     int value {10};
+    int x {100};
+    int y {200};
+    int *int_ptr {nullptr};
 
-    cout << value << endl << endl;
+    cout << "value: " << value << endl << endl;
 
     double_data(&value);
+    cout << "value: " << value << endl << endl;
 
-    cout << value << endl << endl;
+    pass_ptr_in(int_ptr, value);
+    cout << "value: " << value << endl << endl;
+
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+    swap(&x, &y);
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl << endl;
+
     return 0;
 }
 
 
-/*
-Passing pointers to a function
+/*Passing pointers to a function
 -------------------------------------------------
 -Pass-by-reference with pointer parameters
     -We can use pointers and the dereference operator to achieve pass-by-reference
@@ -46,4 +61,20 @@ void double_data(int *int_ptr)
 {
     cout << "double_data() was used\n----------------------------------------" << endl;
     *int_ptr *= 2;
+}
+
+
+void pass_ptr_in(int *int_ptr, int &value)
+{
+    cout << "pass_ptr_in() was used\n----------------------------------------" << endl;
+    int_ptr = &value;
+    *int_ptr *= 2;
+}
+
+void swap(int *a, int *b)
+{
+    cout << "swap() was used\n----------------------------------------" << endl;
+    int temp {*a};
+    *a = *b;
+    *b = temp;
 }
