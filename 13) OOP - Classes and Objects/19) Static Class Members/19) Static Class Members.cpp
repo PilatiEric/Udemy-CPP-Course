b14ef1018b435208b0fe84081d6ea42008bed818
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2025-04-08 21:20:00 -0400

    New Lesson Notes

---
+++ 13) OOP - Classes and Objects/19) Static Class Members/19) Static Class Members.cpp
@@ -0,0 +1,85 @@
+/****************************************************************************************************************
+ * TITLE: Static Class Members - Notes
+ * 
+ * FROM: https://www.udemy.com/course/beginning-c-plus-plus-programming/learn/lecture/9535604#questions
+ * 
+ * SUMMARY: 
+*****************************************************************************************************************/
+
+
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+
+#pragma region Classes
+
+    #include "Player.h"
+    #include "Player.cpp"
+
+#pragma endregion
+
+
+
+
+#pragma region Prototype Functions
+
+    void static_members();
+    void display_active_players();
+
+#pragma endregion
+
+
+
+int main()
+{
+    static_members();
+    return 0;
+}
+
+
+
+
+/*
+Static Class Members
+----------------------------------------------
+-Class data members can be declared as static
+    -A single data member that belongs to the class not the objects
+    -Useful to store class-wide information
+-Class functions can be declared as static
+    -Independent of any objects
+    -Can be called using the class name
+*/
+
+
+void static_members()
+{
+    display_active_players();
+
+    Player obj1 {"Eric"};
+
+    display_active_players();
+
+    {
+        Player person {"Person"};
+        display_active_players();
+    }
+    display_active_players();
+
+    Player *enemy = new Player("Enemy", 100, 100);
+
+    display_active_players();
+    delete enemy;
+    display_active_players();
+}
+
+
+
+void display_active_players()
+{
+    cout << "Active players: " << Player::get_num_players() << endl;
+}
---
+++ 13) OOP - Classes and Objects/19) Static Class Members/Player.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#ifndef _player_h_
+    #define _player_h_
+    class Player
+    {
+        private:
+            static int num_players;
+
+            std::string name;
+            int health;
+            int xp;
+
+        public:
+            std::string get_name() {return name;}
+
+            int get_health() {return health;}
+
+            int get_xp() {return xp;}
+
+            Player();
+            //CONSTRUCTOR
+            Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+
+            //COPY CONSTRUCTOR
+            Player(const Player &source);
+
+            //DESTRUCTOR
+            ~Player();
+
+            static int get_num_players();
+    };
+#endif
